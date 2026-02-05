7e2e4c30eb3c7c36df4539b69224b33e26e46313
Sofiane Toumert <sofiane.toumert@dropkickstud.io>
2026-02-05 00:59:26 +0000

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ["z76gq9-5173.csb.app", "sxzrxk-5173.csb.app"],
+  },
 });
